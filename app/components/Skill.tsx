58d3e57f173ab4b4acf96d1e227bb9d163822692
pakkuu <ethan.pvarghese@gmail.com>
2024-05-16 15:10:11 -0500

    image path changes

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { prefix } from "../../prefix";
 
 interface SkillProps {
   src: string;
@@ -11,7 +10,7 @@ export default function Skill({ src, name }: SkillProps) {
     <div className="shadow-[#00000039] shadow-sm w-[11vw] aspect-square bg-primary border-[0.1vw] border-secondary rounded-[0.6vw] opacity-80 flex flex-col justify-center items-center transition-transform duration-300 hover:scale-105">
       <div className={`${name === "Tailwind" ? "mt-[0vh] mb-[0.1vh]" : ""} mt-0`}>
         <div className="mt-[4vh] mx-auto h-[3.5vw] relative" style={{ width: name === "Tailwind" ? "5vw" : "3.5vw" }}>
-          <Image src={`${prefix}${src}`} alt="" fill sizes="17vw" />
+          <Image src={src} alt="" fill sizes="17vw" />
         </div>
       </div>
       <div className="font-quicksand font-[500] text-accent mt-[3vh] text-center text-[1vw]">
