d47c5010cc555c0807b5b8458f0330ba9ae425e7
pakkuu <ethan.pvarghese@gmail.com>
2024-05-27 18:59:16 -0500

    style fixes

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -12,7 +12,7 @@ export default function AboutMe() {
           I&apos;m a sophmore studying computer science at UTD. In my free time I love to workout, play video games, and watch anime (some of my favorites are Bleach, FMAB, and Samurai Champloo). My interests within computer science lie in Cloud Services, RAG (Retrieval Augmented Generation), AI, and web development, particularly in the backend. I am skilled in C++, Java, Python, HTML/JS/CSS and have had experience with React, NextJS, and AWS.
           </div>
         </div>
-        <div className="shadow-[#00000039] shadow-sm order-[0.1vw] border-secondary rounded-[1.6vw] md:rounded-[0.6vw] mt-[1.5vh] w-full md:w-[23vw] md:ml-[1.8vh] h-[110vw] md:h-[57vh] bg-primary">
+        <div className="shadow-[#00000039] shadow-sm order-[0.1vw] border-secondary rounded-[1.6vw] md:rounded-[0.6vw] mt-[1.5vh] w-full md:w-[23vw] md:ml-[1.8vh] h-[103vw] md:h-[57vh] bg-primary">
         <div className="mt-[3vh] md:mt-[4vh] mx-auto w-[65vw] md:w-[17vw] h-[65vw] md:h-[17vw] relative">
           <Image
             className="shadow-[#00000039] shadow-sm border-[0.07vw] border-secondary rounded-[1.6vw] md:rounded-[0.6vw] opacity-90"
