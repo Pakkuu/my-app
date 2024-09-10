998c56506914b4bd24e8168df6dc58187a953f95
pakkuu <ethan.pvarghese@gmail.com>
2024-09-10 14:51:00 -0500

    modified page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function AboutMe() {
   return (
     <>
       <div className="flex-col sm:mt-[6.0rem] lg:w-[53%] sm:w-[70%] w-[90%] mx-auto lg:pr-[7%] sm:p-0">
-        <div className="font-quicksand text-[#323232d5] font-[400] text-[1.8rem] mr-auto mb-[2rem]">
+        <div className="font-quicksand text-[#323232d5] font-[400] text-[1.8rem] mr-auto mb-[1rem]">
           Hi! My name is Ethan Varghese
         </div>
         <div className="flex relative">
@@ -14,7 +14,7 @@ export default function AboutMe() {
             my favorites are Steins;Gate, FMAB, and Samurai Champloo). My interests
             within computer science lie in Cloud Services, RAG (Retrieval
             Augmented Generation), AI, and full-stack development. I am skilled in C++, Java, Python, HTML/JS/CSS and have
-            experience with React, Next, Express, and AWS!
+            experience with React, Next, Express, and AWS.
           </div>
 
         </div>
